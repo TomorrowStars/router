f9bb573bb4d10b99a8806a209cca207655b45895
TomorrowStars <chenhongsheng2012@163.com>
2017-11-02 17:57:09 +0800

    20171101_chen_重定向路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   // {path: 'product', component: ProductComponent},//在查询参数中传递数据
   // {path: 'product/:id', component: ProductComponent},//在路由路径中传递数据
   {path: 'product', component: ProductComponent, data:[{id:1}]},//在路由配置中传递数据
